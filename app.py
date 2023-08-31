f7e53a499eaeea753a40e4e9bc921e18ef4f1988
Your Name <you@example.com>
2023-08-31 10:25:43 +0300

    game working

--- app.py
+++ app.py
@@ -24,11 +24,13 @@ def index():
 @app.route('/get_sentence')
 def get_sentence():
     all_sentences = Sentence.query.all()
-
     if len(all_sentences) == 0:
+        session['correct'] = 0
+        session['incorrect'] = 0
         return jsonify({'end': True, 'correct': session['correct'], 'incorrect': session['incorrect']})
-
     selected_sentence_record = random.choice(all_sentences)
+    db.session.delete(selected_sentence_record)
+    db.session.commit()
     selected_sentence = {
         'id': selected_sentence_record.id,
         'sentence': selected_sentence_record.sentence,
@@ -39,7 +41,6 @@ def get_sentence():
     }
     formatted_sentence = selected_sentence['sentence'].replace('{}', '___')
     selected_sentence['sentence'] = formatted_sentence
-
     return jsonify(selected_sentence)
 
 @app.route('/check_answer', methods=['POST'])
@@ -48,12 +49,10 @@ def check_answer():
     user_adj_ending = request.form['user_adj_ending']
     correct_article = request.form['correct_article']
     correct_adj_ending = request.form['correct_adj_ending']
-
     if user_article == correct_article and user_adj_ending == correct_adj_ending:
         session['correct'] += 1
     else:
         session['incorrect'] += 1
-
     return jsonify({'correct': session['correct'], 'incorrect': session['incorrect']})
 
 if __name__ == '__main__':
