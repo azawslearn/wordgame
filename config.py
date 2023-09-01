4845966a11ed72cf936f1e1f16a243d7c09caffe
Your Name <you@example.com>
2023-09-01 14:08:51 +0300

    same app as azure

--- config.py
+++ config.py
@@ -1,6 +1,10 @@
 import os
+
 # Database configurations
 DB_USERNAME = "ivansto"
 DB_PASSWORD = "EmersonFitipaldi"
 DB_HOST = os.environ.get('VM2_IP', 'default_value_if_not_set')
 DB_NAME = "words"
+
+
+
