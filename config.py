2df9e681b02cef2c59415ed1dcc53a4823c0e8ba
azawslearn <134204024+azawslearn@users.noreply.github.com>
2023-09-01 11:48:58 +0300

    Update config.py

--- config.py
+++ config.py
@@ -1,5 +1,6 @@
+import os
 # Database configurations
 DB_USERNAME = "ivansto"
 DB_PASSWORD = "EmersonFitipaldi"
-DB_HOST = "172.174.76.187"
+DB_HOST = os.environ.get('VM2_IP', 'default_value_if_not_set')
 DB_NAME = "words"
