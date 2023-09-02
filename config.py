87c2d8874b917c06697eed3fcba381ee54fef0b7
azawslearn <134204024+azawslearn@users.noreply.github.com>
2023-09-02 15:20:07 +0300

    Update config.py

--- config.py
+++ config.py
@@ -1,10 +1,11 @@
-import os
+import configparser
+
+# Reading config.cfg file
+config = configparser.ConfigParser()
+config.read('config.cfg')
 
 # Database configurations
 DB_USERNAME = "ivansto"
 DB_PASSWORD = "EmersonFitipaldi"
-DB_HOST = os.environ.get('VM2_IP', 'default_value_if_not_set')
+DB_HOST = config.get('Database', 'DB_HOST', fallback='default_value_if_not_set')
 DB_NAME = "words"
-
-
-
