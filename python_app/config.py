ac8edff2ae2a00fb8bc1c708d58f33c1b6a642f3
Your Name <you@example.com>
2023-09-06 15:59:12 +0300

    Terraform5

--- python_app/config.py
+++ python_app/config.py
@@ -8,7 +8,7 @@ credential = DefaultAzureCredential()
 secret_client = SecretClient(vault_url="https://<YourVaultName>.vault.azure.net/", credential=credential)
 
 # Retrieve secrets from Azure Key Vault
-DB_USERNAME = secret_client.get_secret("DB_USERNAME").value
-DB_PASSWORD = secret_client.get_secret("DB_PASSWORD").value
-DB_HOST = secret_client.get_secret("DB_HOST").value or 'default_value_if_not_set'
-DB_NAME = secret_client.get_secret("DB_NAME").value
+DB_USERNAME = secret_client.get_secret("DBUSERNAME").value
+DB_PASSWORD = secret_client.get_secret("DBPASSWORD").value
+DB_HOST = secret_client.get_secret("DBHOST").value or 'default_value_if_not_set'
+DB_NAME = secret_client.get_secret("DBNAME").value
