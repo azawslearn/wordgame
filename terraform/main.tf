c6b51ff3032a37c673538fd9ca49856e81c0b895
Your Name <you@example.com>
2023-09-06 11:45:08 +0300

    fixing the terraform

--- terraform/main.tf
+++ terraform/main.tf
@@ -11,14 +11,14 @@ provider "azurerm" {
 }
 
 resource "azurerm_resource_group" "dockerWOrdGame" {
-  name     = "functionRG"
+  name     = "dockerWOrdGame"
   location = "West Europe"
 }
 
 resource "azurerm_mysql_flexible_server" "docker_mysql" {
   name                   = "docker_mysql"
-  resource_group_name    = azurerm_resource_group.functionRG.name
-  location               = azurerm_resource_group.functionRG.location
+  resource_group_name    = azurerm_resource_group.dockerWOrdGame.name
+  location               = azurerm_resource_group.dockerWOrdGame.location
   administrator_login    = "mysqladminun"
   administrator_password = "EmersonFitipaldi1!"
   sku_name               = "B_Standard_B1s"
@@ -26,17 +26,17 @@ resource "azurerm_mysql_flexible_server" "docker_mysql" {
 }
 
 resource "azurerm_mysql_flexible_database" "docker_db" {
-  name                = "users"
-  resource_group_name = azurerm_resource_group.functionRG.name
-  server_name         = azurerm_mysql_flexible_server.functionMySQLServer.name
+  name                = "docker_db"
+  resource_group_name = azurerm_resource_group.dockerWOrdGame.name
+  server_name         = azurerm_mysql_flexible_server.docker_mysql.name
   charset             = "utf8"
   collation           = "utf8_unicode_ci"
 }
 
 resource "azurerm_mysql_flexible_server_firewall_rule" "allow_all_ips" {
   name                = "allow-all-ips-rule"
-  resource_group_name = azurerm_resource_group.functionRG.name
-  server_name         = azurerm_mysql_flexible_server.functionMySQLServer.name
+  resource_group_name = azurerm_resource_group.dockerWOrdGame.name
+  server_name         = azurerm_mysql_flexible_server.docker_mysql.name
   start_ip_address    = "0.0.0.0"
   end_ip_address      = "255.255.255.255"
   depends_on          = [azurerm_mysql_flexible_server.functionMySQLServer]
@@ -46,4 +46,5 @@ output "mysql_server_fqdn" {
   description = "The Fully Qualified Domain Name of the MySQL Server."
   value       = "${azurerm_mysql_flexible_server.docker_mysql.name}.mysql.database.azure.com"
 }
+
 
